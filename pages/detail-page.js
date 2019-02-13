cf734ca0ac39c2869b8936d8728f9552ced51c26
KA878 <karim.a.aziz@gmail.com>
2019-02-13 17:24:41 +0100

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -5,26 +5,34 @@ const submitComment = function() {
     
     const msgInput = document.getElementById('msg');
     const msg = msgInput.value;
+    
     const comment = document.createElement('section');
-    const dispInput = document.createElement('h3');
-    const dispMsg = document.createElement('p');
-    
-    const h3 = name;
-    const p = msg;
-
-    
+    const h3 = document.createElement('h3');
+    const p = document.createElement('p');
 
     console.log(name);
     console.log(msg);
-    console.log(h3 + ' said: ');
-    console.log(' " ' + p + ' " ');   
+    console.log(name + ' said: ');
+    console.log(' " ' + msg + ' " ');   
+
+    h3.innerHTML =  (name + ' said: ');
+    p.innerHTML = msg;
     
     comment.class = ('comment');
     console.log(comment.class);  
     
-    comment.appendChild(dispInput);   
-    comment.appendChild(dispMsg);
+    comment.appendChild(h3);   
+    comment.appendChild(p);
+
+    const commentSection = document.getElementById('comments');
+    commentSection.appendChild(comment);
     console.log(comment);
-     
+    
+    //console.log(commentSection.name);
+    //console.log(commentSection.msg);
+
+    //h3.value = null;
+    //p.value = null;
+
     
 }
