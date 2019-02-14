a0191fd5a0012e84bb6caa29e15bc0ec7e180a71
KA878 <karim.a.aziz@gmail.com>
2019-02-14 14:15:33 +0100

    add validations

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -12,27 +12,50 @@ const submitComment = function() {
 
     console.log(name);
     console.log(msg);
-    console.log(name + ' said: ');
-    console.log(' " ' + msg + ' " ');   
+    //console.log(name + ' said: ');
+    //console.log(' " ' + msg + ' " ');   
 
     h3.innerHTML =  (name + ' said: ');
     p.innerHTML = msg;
     
     comment.class = ('comment');
-    console.log(comment.class);  
+    //console.log(comment.class);  
     
     comment.appendChild(h3);   
     comment.appendChild(p);
 
     const commentSection = document.getElementById('comments');
     commentSection.appendChild(comment);
-    console.log(comment);
+    //console.log(comment);
     
     //console.log(commentSection.name);
     //console.log(commentSection.msg);
 
-    h3.value = null;
-    p.value = null;
+    inputField.value = null;
+    msgInput.value = null;
 
-    
+    const doesNotPassAllValidations = function () {
+        //const longName = name.length;
+        //const longMsg = msg.length;        
+        
+        if (!name || !msg) {
+            alert("Don't forget to input your name and message");
+            return true;
+        }
+        
+        if (msg.length > 280) {
+            alert('Your comment is too long!');
+            return true;
+        }
+
+        else {
+            return false;
+        }
+    }
+    //doesNotPassAllValidations();
+    if (doesNotPassAllValidations() === true) {
+        return null;
+    }
 }
+//console.log(name);
+//console.log(msg);
